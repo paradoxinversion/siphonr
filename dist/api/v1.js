8db15c7be5c0e5f2f7df76535c0142e05ab83004
Jedai Saboteur <dev@paradoxinversion.com>
2018-04-10 16:15:12 -0700

    Added cors to search route

--- dist/api/v1.js
+++ dist/api/v1.js
@@ -15,7 +15,16 @@ var _passport2 = _interopRequireDefault(_passport);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var router = _express2.default.Router();
-
+router.use(function (req, res, next) {
+  console.log(process.env.NODE_ENV);
+  var origin = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "http://siphonr-overview.herokuapp.com";
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Expose-Headers", "x-auth-token");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 router.route("/search").get(_passport2.default.authenticate("twitter-token", { session: true }), _search2.default);
 
 module.exports = router;
--- src/api/v1.js
+++ src/api/v1.js
@@ -2,7 +2,16 @@ import express from 'express';
 import search from "./search";
 import passport from "passport";
 const router = express.Router();
-
+router.use(function(req, res, next) {
+  console.log(process.env.NODE_ENV);
+  const origin = (process.env.NODE_ENV === "development") ? "http://localhost:3001" : "http://siphonr-overview.herokuapp.com"; 
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Expose-Headers", "x-auth-token");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 router.route("/search")
   .get(passport.authenticate("twitter-token", {session: true}),search);
 
